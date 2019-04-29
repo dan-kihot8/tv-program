1cd3ce6e8189ea6fa204d250d66e547036c769c6
dan <yarushind@gmail.com>
2019-04-29 19:34:27 +0500

    Updates

---
+++ precache-manifest.abafdb95d901aa782966ffec3fbb2c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc65864b05e74265d7b8ec52fc29575a",
+    "url": "/tv-program/index.html"
+  },
+  {
+    "revision": "496980a21d73758a7b2b",
+    "url": "/tv-program/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "308123754477ba549b0a",
+    "url": "/tv-program/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "496980a21d73758a7b2b",
+    "url": "/tv-program/static/js/2.4be12ac0.chunk.js"
+  },
+  {
+    "revision": "308123754477ba549b0a",
+    "url": "/tv-program/static/js/main.626b4a82.chunk.js"
+  },
+  {
+    "revision": "260e3058c41cf9f7824a",
+    "url": "/tv-program/static/js/runtime~main.c2d62955.js"
+  }
+]);
